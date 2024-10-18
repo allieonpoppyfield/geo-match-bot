0486e1b5bc0a1ae0285d8b4ae0e9a038a76b76d3
mishastalinaa <mishastalinaa@yandex.ru>
2024-10-18 23:01:51 +0300

    test from hp

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -6,6 +6,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+// NewBot ..
 func NewBot(token string) (*tgbotapi.BotAPI, error) {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
